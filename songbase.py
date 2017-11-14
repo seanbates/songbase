c82d8147ddc851293a97c1dbb6ef1157c9991319
Sean Bates <sbates@udel.edu>
2017-11-14 10:44:24 -0500

    added web form

--- songbase.py
+++ songbase.py
@@ -5,3 +5,13 @@ def index():
 @app.route('/user/<string:name>/')
 def get_user(name):
     return 'hello %s %d' % (name, 3)
+
+@app.route('/form-demo', methods=['GET', 'POST'])
+def form_demo():
+    if request.method=='GET':
+        first_name = request.args.get_user('first_name')
+        return render_template('form-demo.html')
+
+    if request.method=='POST':
+        first_name = request.form['first_name']
+        return render_template('form-demo.html')
